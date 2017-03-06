45edae93f70d2870969555f34383321bf6b74171
Lei Chen <chenlei@smartalpha.cn>
2017-03-06 09:53:49 +0800

    .

--- base/api/common/base/resolver.js
+++
@@ -1,6 +0,0 @@
-/**
- * base Resolver, you should create a subclass extended it.
- */
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=resolver.js.map
--- workbench/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
-var appmodule_1 = require("./appmodule");
-// enableProdMode();
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(appmodule_1.AppModule);
-//# sourceMappingURL=main.js.map
