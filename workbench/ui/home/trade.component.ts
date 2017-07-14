227a8d8c151f0008b49bb5fe47f136ab9e983eda
xinkui <xinkui@smartalpha.cn>
2017-07-14 14:06:47 +0800

    temp comit

--- workbench/ui/home/trade.component.ts
+++ workbench/ui/home/trade.component.ts
@@ -1,22 +1,34 @@
 "use strict";
 
 import { Component } from "@angular/core";
+// import { IP20Service } from "../../../base/api/services/ip20.service";
+import { ConfigurationBLL, StrategyServerContainer, WorkspaceConfig, Channel, StrategyInstance, SpreadViewConfig } from "../../bll/strategy.server";
 
 @Component({
     moduleId: module.id,
     selector: "trade",
     templateUrl: "trade.component.html",
-    styleUrls: ["trade.component.css"]
+    styleUrls: ["trade.component.css"],
+    providers: []
 })
 export class TradeComponent {
     styleObj: any;
     dataSource: any;
     activeTab = "Dashboard";
-
+    selectedServer: any;
+    private configBLL = new ConfigurationBLL();
+    private strategyContainer = new StrategyServerContainer();
     constructor() {
+        console.log("in trading page");
+        // load product,strategy and analytical
+        this.loginTGW();
 
     }
 
     ngOnInit() {
+    }
+    loginTGW(): void {
+        // get template back
+        //  create back test channal
     }
 }
