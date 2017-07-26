81e27039146cfd6f23e39ba0839c3da2166d051a
Xiaobo Mu <muxiaobo@hongkingsystem.com>
2017-07-26 13:45:57 +0800

    修改了图表的样式

--- workbench/ui/home/riskfactor.component.ts
+++ workbench/ui/home/riskfactor.component.ts
@@ -629,6 +629,27 @@ export class RiskFactorComponent {
                 scale: true,
                 boundaryGap: [0.2, 0.2]
             }],
+            dataZoom: [{
+    					type: 'inside',
+    					xAxisIndex: 0 ,
+    					start: 0,
+    					end: 100
+    				}, {
+    						start: 0,
+    						end: 10,
+    						handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+    						handleSize: '60%',
+                textStyle: {
+                  color: "#FFF"
+                }
+    						handleStyle: {
+    								color: '#fff',
+    								shadowBlur: 3,
+    								shadowColor: 'rgba(0, 0, 0, 0.6)',
+    								shadowOffsetX: 2,
+    								shadowOffsetY: 2
+    						}
+    				}],
             series: series
             // color: [
             //     "#00b", "#0b0"
@@ -680,6 +701,27 @@ export class RiskFactorComponent {
                   scale: true,
                   boundaryGap: [0.2, 0.2]
               },
+              dataZoom: [{
+      					type: 'inside',
+      					xAxisIndex: 0 ,
+      					start: 0,
+      					end: 100
+      				}, {
+      						start: 0,
+      						end: 10,
+      						handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      						handleSize: '60%',
+                  textStyle: {
+                    color: "#FFF"
+                  }
+      						handleStyle: {
+      								color: '#fff',
+      								shadowBlur: 3,
+      								shadowColor: 'rgba(0, 0, 0, 0.6)',
+      								shadowOffsetX: 2,
+      								shadowOffsetY: 2
+      						}
+      				}],
               series: [{
                       name: "风险因子收益",
                       type: "bar",
@@ -770,6 +812,27 @@ export class RiskFactorComponent {
                   scale: true,
                   boundaryGap: [0.2, 0.2]
               },
+              dataZoom: [{
+      					type: 'inside',
+      					xAxisIndex: 0 ,
+      					start: 0,
+      					end: 100
+      				}, {
+      						start: 0,
+      						end: 10,
+      						handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      						handleSize: '60%',
+                  textStyle: {
+                    color: "#FFF"
+                  }
+      						handleStyle: {
+      								color: '#fff',
+      								shadowBlur: 3,
+      								shadowColor: 'rgba(0, 0, 0, 0.6)',
+      								shadowOffsetX: 2,
+      								shadowOffsetY: 2
+      						}
+      				}],
               series: everydayExposureSeries
               // color: [
               //     "#00b", "#0b0"
@@ -822,6 +885,27 @@ export class RiskFactorComponent {
                   scale: true,
                   boundaryGap: [0.2, 0.2]
               },
+              dataZoom: [{
+      					type: 'inside',
+      					xAxisIndex: 0 ,
+      					start: 0,
+      					end: 100
+      				}, {
+      						start: 0,
+      						end: 10,
+      						handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      						handleSize: '60%',
+                  textStyle: {
+                    color: "#FFF"
+                  }
+      						handleStyle: {
+      								color: '#fff',
+      								shadowBlur: 3,
+      								shadowColor: 'rgba(0, 0, 0, 0.6)',
+      								shadowOffsetX: 2,
+      								shadowOffsetY: 2
+      						}
+      				}],
               series: [{
                       name: "风险因子收益",
                       type: "bar",
@@ -909,6 +993,27 @@ export class RiskFactorComponent {
                   scale: true,
                   boundaryGap: [0.2, 0.2]
               },
+              dataZoom: [{
+      					type: 'inside',
+      					xAxisIndex: 0 ,
+      					start: 0,
+      					end: 100
+      				}, {
+      						start: 0,
+      						end: 10,
+      						handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      						handleSize: '60%',
+                  textStyle: {
+                    color: "#FFF"
+                  }
+      						handleStyle: {
+      								color: '#fff',
+      								shadowBlur: 3,
+      								shadowColor: 'rgba(0, 0, 0, 0.6)',
+      								shadowOffsetX: 2,
+      								shadowOffsetY: 2
+      						}
+      				}],
               series: everyDayReturnAttrSeries
               // color: [
               //     "#00b", "#0b0"
@@ -962,6 +1067,48 @@ export class RiskFactorComponent {
                   scale: true,
                   boundaryGap: [0.2, 0.2]
               },
+              dataZoom: [{
+      					type: 'inside',
+      					xAxisIndex: 0 ,
+      					start: 0,
+      					end: 100
+      				}, {
+      						start: 0,
+      						end: 10,
+      						handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      						handleSize: '60%',
+                  textStyle: {
+                    color: "#FFF"
+                  }
+      						handleStyle: {
+      								color: '#fff',
+      								shadowBlur: 3,
+      								shadowColor: 'rgba(0, 0, 0, 0.6)',
+      								shadowOffsetX: 2,
+      								shadowOffsetY: 2
+      						}
+      				}],
+              dataZoom: [{
+      					type: 'inside',
+      					xAxisIndex: 0 ,
+      					start: 0,
+      					end: 100
+      				}, {
+      						start: 0,
+      						end: 10,
+      						handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+      						handleSize: '60%',
+                  textStyle: {
+                    color: "#FFF"
+                  }
+      						handleStyle: {
+      								color: '#fff',
+      								shadowBlur: 3,
+      								shadowColor: 'rgba(0, 0, 0, 0.6)',
+      								shadowOffsetX: 2,
+      								shadowOffsetY: 2
+      						}
+      				}],
               series: [{
                       name: "风险因子归因",
                       type: "bar",
