5ae28dd864db51a5ed1ddf2818159d9a50b90463
Lei Chen <chenlei@smartalpha.cn>
2017-08-02 10:58:30 +0800

    add exit feature; update dockdemo style; update index market data chart; update risk page; hanizate main page;

--- workbench/ui/home/risk.component.ts
+++ workbench/ui/home/risk.component.ts
@@ -2,22 +2,29 @@
 
 import { Component } from "@angular/core";
 import { TradeService } from "../../bll/services";
+import { DataTable } from "../../../base/controls/control";
 
 @Component({
     moduleId: module.id,
     selector: "risk",
-    template: "risk",
+    templateUrl: "risk.html",
     styleUrls: ["home.component.css"]
 })
 export class RiskComponent {
-    styleObj: any;
-    dataSource: any;
+    strategyTable: DataTable;
+    accountTable: DataTable;
 
     constructor(private trade: TradeService) {
 
     }
 
     ngOnInit() {
+        this.strategyTable = new DataTable();
+        this.strategyTable.addColumn("名称", "当前值", "阈值");
+
+        this.accountTable = new DataTable();
+        this.accountTable.addColumn("名称", "当前值", "阈值");
+
         this.registerListeners();
         this.trade.send(130, 2001, {});
     }
