d936a67f279c4ab3d64a3d3806485738aadb85b7
Lei Chen <chenlei@smartalpha.cn>
2017-08-02 22:08:55 +0800

    update product page; update tilearea control;

--- appstore/Dialog/product/product.ts
+++ appstore/Dialog/product/product.ts
@@ -2,6 +2,7 @@
 
 import { Component, OnInit } from "@angular/core";
 import { IP20Service } from "../../../base/api/services/ip20.service";
+import { DataTable } from "../../../base/controls/control";
 
 @Component({
     moduleId: module.id,
@@ -11,18 +12,25 @@ import { IP20Service } from "../../../base/api/services/ip20.service";
 })
 export class ProductComponent implements OnInit {
     productDetail: Table;
+    futureTable: DataTable;
+    stockTable: DataTable;
 
     constructor(private tgw: IP20Service) {
     }
 
     ngOnInit() {
         this.productDetail = new Table();
-        this.productDetail.rows[0] = [];
-        this.productDetail.rows.push("名称", "总资金规模", "昨日净值", "当日净值");
-        this.productDetail.rows.push("平仓盈亏", "浮动盈亏", "保证金", "股票账户可用资金");
-        this.productDetail.rows.push("期货账户可用资金", "期货账户总权益", "股票总市值", "股指期货总合约价值");
-        this.productDetail.rows.push("总敞口", "总资金规模", "昨日净值", "当日净值");
+        this.productDetail.rows.push(["名称", "1000", "总资金规模", "1000", "昨日净值", "1000", "当日净值", "1000"]);
+        this.productDetail.rows.push(["平仓盈亏", "1000", "浮动盈亏", "1000", "保证金", "1000", "股票账户可用资金"]);
+        this.productDetail.rows.push(["期货账户可用资金", "1000", "期货账户总权益", "1000", "股票总市值", "1000", "股指期货总合约价值"]);
+        this.productDetail.rows.push(["股票及股指期货总敞口", "1000", "商品期货总合约价值", "1000", "商品期货总合约轧差", "1000", "期货账户风险度"]);
 
+        this.futureTable = new DataTable("table2");
+        this.futureTable.addColumn("代码", "名称", "持仓量", "持仓金额");
+        this.futureTable.backgroundColor = "transparent";
+        this.stockTable = new DataTable("table2");
+        this.stockTable.addColumn("代码", "名称", "持仓量", "持仓金额");
+        this.stockTable.backgroundColor = "transparent";
         this.registerListener();
     }
 
