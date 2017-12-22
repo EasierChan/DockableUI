5543b73e531d2b3e576575f13678df179cd9134a
Lei Chen <chenlei@smartalpha.cn>
2017-12-22 15:05:22 +0800

    strategy instance with different tabs;

--- appstore/DockDemo/startup.ts
+++ appstore/DockDemo/startup.ts
@@ -223,9 +223,7 @@ export class StartUp implements IApplication {
                     type: "v",
                     width: width - Math.floor(width * 0.3) - 5,
                     modules: [
-                        "Log",
-                        "StatArb",
-                        "Portfolio"
+                        "Log"
                     ]
                 }]
             }]
@@ -234,11 +232,11 @@ export class StartUp implements IApplication {
         switch (this._option.sstype) {
             case "portfoliotrader":
                 // portfolio
-                (res as any).children[2].children[1].modules = ["Log", "Portfolio"];
+                (res as any).children[2].children[1].modules.push("Portfolio");
                 break;
             case "pairtrader":
                 // pairtrade
-                (res as any).children[2].children[1].modules = ["Log", "StatArb"];
+                (res as any).children[2].children[1].modules.push("StatArb");
                 break;
             default:
                 break;
